975b9f62470fd2896f23b4a0dfa2e41a011e0c7d
FValenzuelaB <164669852+FValenzuelaB@users.noreply.github.com>
2025-04-27 00:35:46 +0000

    listo hasta preparacion falta color en click

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$('#enviarporcorreo').click(function(){
+    alert("El correo fue enviado correctamente!")
+})
